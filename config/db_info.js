60cc7202e1725b8db303b84795194fb7b5288083
KoreanWarrior <bbcjswo@naver.com>
2017-08-18 18:28:49 +0900

    최신 웹

---
+++ config/db_info.js
@@ -0,0 +1,25 @@
+module.exports = (function () {
+  return {
+    local: { // localhost
+      host: 'localhost',
+      port: '3306',
+      user: 'root',
+      password: '',
+      database: ''
+    },
+    real: { // real server db info
+      host: '192.168.0.155',
+      port: '3306',
+      user: 'citymanage',
+      password: 'sjj',
+      database: 'citymanage'
+    },
+    dev: { // dev server db info
+      host: '',
+      port: '',
+      user: '',
+      password: '',
+      database: ''
+    }
+  }
+})();
--- routes/api.js
+++ routes/api.js
@@ -1,33 +1,118 @@
 var express = require('express');
-var mysql = require('mysql');
-var con = mysql.createConnection({
-  host: "192.168.0.155",
-  user: "citymanage",
-  password: "sjj",
-  database:"citymanage"
-});
+var mysql_dbc = require('../config/db_con')();
+var connection = mysql_dbc.init();
+mysql_dbc.test_open(connection);
 
+function Result (resultCode, resultMessage) {
+    this.resultCode = resultCode;
+    this.resultMessage = resultMessage;
+}
 
+Result.prototype.getInfo = function() {
+    return this.resultCode + ' ' + this.resultMessage + ' result';
+};
 
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/memberLogin.app', function(req, res, next) {
-  con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
 
-    var sql = "select * from member";
-    con.query(sql, function (err, result) {
-      if (err) throw err;
-      console.log("Result: " + result);
-      res.json(result);
-    });
+  var reqParam = req.query;
+
+  var sqlQueryParam = [reqParam.memberId, reqParam.memberPwd];
+  var sqlQuery = 'select count(*) as count from member where member_id = ? and member_pwd = ?';
+  connection.query(sqlQuery, sqlQueryParam, function (err, row, fields) {
+    if(err){
+      res.send('error');
+    } else {
+
+      var result = new Result();
+
+      console.log(row);
+      console.log(row[0].count);
+
+      if(row[0].count === 1) {
+        result.resultCode = '200';
+        result.resultMessage = '로그인을 환영합니다.';
+
+      } else {
+        result.resultCode = '500';
+        result.resultMessage = '회원정보가 없습니다.';
+      }
+      res.statusCode = "200";
+  		res.setHeader('Content-Type', 'application/json;');
+  		res.end(JSON.stringify(result, null, 3));
+    }
   });
 });
 
 router.get('/memberRegister.app', function(req, res, next) {
-  res.send('memberRegister.app');
+  // http://192.168.0.155:3000/memberRegister.app?memberId=test4&memberPwd=test4&memberPhone=01011111111&memberName=park&memberPhoto=jelly.jpg&memberEmail=bbcjswo@naver.com
+  var reqParam = req.query;
+
+  var memberId = reqParam.memberId;
+  var memberPwd = reqParam.memberPwd;
+  var memberName = reqParam.memberName;
+  var memberPhone = reqParam.memberPhone;
+  var memberPhoto = reqParam.memberPhoto;
+  var memberEmail = reqParam.memberEmail;
+
+  var sqlQueryParam = [memberId, memberPwd, memberName, memberPhone, memberPhoto, memberEmail];
+  var sqlQuery = "insert into member("
+                +"member_id,member_pwd,member_name,member_phone,member_email,member_photo,member_authorization) "
+                +"values(?,?,?,?,?,?,'app_user')";
+  connection.query(sqlQuery, sqlQueryParam, function (err, row, fields) {
+
+    var result = new Result();
+    if(err){
+      result.resultCode = '500';
+      result.resultMessage = '회원 가입이 실패하였습니다.';
+    } else {
+      if(row.insertId === 0) {
+        result.resultCode = '200';
+        result.resultMessage = '회원 가입이 되었습니다.';
+
+      } else {
+        result.resultCode = '500';
+        result.resultMessage = '회원 가입이 실패하였습니다.';
+      }
+    }
+    res.json(result);
+  });
+});
+
+router.get('/cityStateInfoRegister.app', function(req, res, next) {
+  // http://192.168.0.155:3000/api/cityStateInfoRegister.app?memberId=test4&memberPwd=test4&cityGeoCode=01&stateGeoCode=02
+  var reqParam = req.query;
+
+  var memberId = reqParam.memberId;
+  var memberPwd = reqParam.memberPwd;
+  var cityGeoCode = reqParam.cityGeoCode;
+  var stateGeoCode = reqParam.stateGeoCode;
+
+  var sqlQueryParam = [cityGeoCode, stateGeoCode, memberId, memberPwd  ];
+  var sqlQuery = "update member set"
+                +" city_geocode=?, state_geocode=? where member_id=? and member_pwd=? ";
+  connection.query(sqlQuery, sqlQueryParam, function (err, row, fields) {
+    var result = new Result();
+
+    if(err){
+      result.resultCode = '500';
+      result.resultMessage = '시스템 관리자에게 문의 하세요';
+    } else {
+      if(row.insertId === 0) {
+        result.resultCode = '200';
+        result.resultMessage = '회원 가입이 되었습니다.';
+
+      } else {
+        result.resultCode = '400';
+        result.resultMessage = '회원 가입이 실패하였습니다.';
+      }
+    }
+    res.statusCode = "200";
+		res.setHeader('Content-Type', 'application/json;');
+		res.end(JSON.stringify(result, null, 3));
+  });
 });
 
 router.get('/memberPwdConfirm.app', function(req, res, next) {
@@ -90,57 +175,12 @@ router.get('/favoritesRegister.app', function(req, res, next) {
   res.send('favoritesRegister.app');
 });
 
-router.get('/generousRegister.app', function(req, res, next) {
-  res.send('generousRegister.app');
-});
-
-router.get('/stinkRegister.app', function(req, res, next) {
-  res.send('stinkRegister.app');
-});
-
-router.get('/generousRegister.app', function(req, res, next) {
-  res.send('generousRegister.app');
-});
-
-router.get('/flameDetectionRegister.app', function(req, res, next) {
-  res.send('flameDetectionRegister.app');
-});
-
-
-router.get('/lockRegister.app', function(req, res, next) {
-  res.send('lockRegister.app');
-});
-
-router.get('/flameDetectionRegister.app', function(req, res, next) {
-  res.send('flameDetectionRegister.app');
-});
-
-router.get('/waterLevelRegister.app', function(req, res, next) {
-  res.send('waterLevelRegister.app');
-});
-
-router.get('/waterQualityRegister.app', function(req, res, next) {
-  res.send('waterQualityRegister.app');
-});
-
-router.get('/gasDensityRegister.app', function(req, res, next) {
-  res.send('gasDensityRegister.app');
-});
-
-router.get('/shockDetectionNotice.app', function(req, res, next) {
-  res.send('shockDetectionNotice.app');
-});
-
-router.get('/smokeDetectionNotice.app', function(req, res, next) {
-  res.send('smokeDetectionNotice.app');
+router.get('/sensorInfoRegister.app', function(req, res, next) {
+  res.send('sensorInfoRegister.app');
 });
 
 router.get('/operationStatusRegister.app', function(req, res, next) {
   res.send('operationStatusRegister.app');
-});
-
-router.get('/dangerRegister.app', function(req, res, next) {
-  res.send('dangerRegister.app');
 });
 
 router.get('/pushActionRegister.app', function(req, res, next) {
