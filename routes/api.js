62f2cfe37474070207d14eb5826f38b3bcfb3aa5
KoreanWarrior <bbcjswo@naver.com>
2017-08-24 17:43:15 +0900

    2017.08.24

--- routes/api.js
+++ routes/api.js
@@ -29,9 +29,6 @@ router.get('/memberLogin.app', function(req, res, next) {
 
       var result = new Result();
 
-      console.log(row);
-      console.log(row[0].count);
-
       if(row[0].count === 1) {
         result.resultCode = '200';
         result.resultMessage = '로그인을 환영합니다.';
@@ -71,9 +68,6 @@ router.get('/cityInfo.app', function(req, res, next) {
         res.setHeader('Content-Type', 'application/json; charset=utf-8');
 
         var cityStr = {"resultCode":"200", "resultMessage":"조회", "city":row};
-
-        console.log(JSON.stringify(cityStr,null,3));
-
         res.json(cityStr);
 
 
@@ -100,23 +94,12 @@ router.get('/stateInfo.app', function(req, res, next) {
 	} else {
 
 		var result = new Result();
-		var dataLength = row.length;
 
-	  if(dataLength > 0) {
 		res.statusCode = "200";
 		res.setHeader('Content-Type', 'application/json; charset=utf-8');
 
 		var stateStr = {"resultCode":"200", "resultMessage":"조회", "state":row};
-		
-		console.log(JSON.stringify(stateStr,null,3));
-		
 		res.json(stateStr);
-
-		} else {
-		  result.resultCode = '500';
-		  result.resultMessage = '시/도 정보 조회 실패 하였습니다.';
-		  res.json(result);
-		}
 	}
   });
 });
@@ -144,7 +127,7 @@ router.get('/memberRegister.app', function(req, res, next) {
       result.resultCode = '500';
       result.resultMessage = '회원 가입이 실패하였습니다.';
     } else {
-      if(row.insertId === 0) {
+      if(row.affectedRows === 1) {
         result.resultCode = '200';
         result.resultMessage = '회원 가입이 되었습니다.';
 
@@ -178,7 +161,7 @@ router.get('/cityStateInfoRegister.app', function(req, res, next) {
       result.resultCode = '500';
       result.resultMessage = '시스템 관리자에게 문의 하세요';
     } else {
-      if(row.insertId === 0) {
+      if(row.affectedRows === 1) {
         result.resultCode = '200';
         result.resultMessage = '회원 정보가 변경 되었습니다.';
 
@@ -188,29 +171,192 @@ router.get('/cityStateInfoRegister.app', function(req, res, next) {
       }
     }
     res.statusCode = "200";
-		res.setHeader('Content-Type', 'application/json;');
-		res.end(JSON.stringify(result, null, 3));
+	res.setHeader('Content-Type', 'application/json; charset=utf-8');
+	res.end(JSON.stringify(result, null, 3));
   });
 });
 
 router.get('/memberPwdConfirm.app', function(req, res, next) {
-  res.send('memberPwdConfirm.app');
+  var reqParam = req.query;
+
+  var memberId = reqParam.memberId;
+  var memberPwd = reqParam.memberPwd;
+  
+  var sqlQueryParam = [memberId, memberPwd];
+  var sqlQuery = "select count(*) from where member_id=? and member_pwd=?";
+  connection.query(sqlQuery, sqlQueryParam, function (err, row, fields) {
+    var result = new Result();
+
+    if(err){
+      result.resultCode = '500';
+      result.resultMessage = '시스템 관리자에게 문의 하세요';
+    } else {
+      if(row[0].count === 1) {
+        result.resultCode = '200';
+        result.resultMessage = '회원 정보가 확인 되었습니다.';
+
+      } else {
+        result.resultCode = '400';
+        result.resultMessage = '회원 정보 확인이 실패하였습니다.';
+      }
+    }
+    res.statusCode = "200";
+	res.setHeader('Content-Type', 'application/json; charset=utf-8');
+	res.end(JSON.stringify(result, null, 3));
+  });
 });
 
 router.get('/memberPwdChange.app', function(req, res, next) {
-  res.send('memberPwdChange.app');
+  var reqParam = req.query;
+
+  var memberId = reqParam.memberId;
+  var memberPwd = reqParam.memberPwd;
+  var memberChangePwd = reqParam.memberChangePwd;
+  
+  var sqlQueryParam = [memberChangePwd, memberId, memberPwd];
+  var sqlQuery = "update member set member_pwd=? where member_id=? and member_pwd=? ";
+  connection.query(sqlQuery, sqlQueryParam, function (err, row, fields) {
+    var result = new Result();
+
+    if(err){
+      result.resultCode = '500';
+      result.resultMessage = '시스템 관리자에게 문의 하세요';
+    } else {
+      if(row.affectedRows === 1) {
+        result.resultCode = '200';
+        result.resultMessage = '회원 정보가 변경 되었습니다.';
+
+      } else {
+        result.resultCode = '400';
+        result.resultMessage = '회원 정보 변경이 실패하였습니다.';
+      }
+    }
+    res.statusCode = "200";
+	res.setHeader('Content-Type', 'application/json; charset=utf-8');
+	res.end(JSON.stringify(result, null, 3));
+  });
 });
 
+/* 미정 */
 router.get('/memberProfileImageChange.app', function(req, res, next) {
   res.send('memberProfileImageChange.app');
 });
 
+/* 완료 */
+router.get('/favoritesRegister.app', function(req, res, next) {
+  	var reqParam = req.query;
+	
+	var memberId = reqParam.memberId;
+	var manageId = reqParam.manageId;
+	
+	console.log(memberId);
+	console.log(manageId);
+
+	var sqlQueryParam = [manageId,memberId];
+	var sqlQuery = "insert into favorites_info(manage_id, bookmark, member_id ) values(?, 'Y', ?)";
+	connection.query(sqlQuery, sqlQueryParam, function (err, row, fields) {
+		
+	var result = new Result();
+		
+	if(err){
+	  result.resultCode = '500';
+      result.resultMessage = '시스템 관리자에게 문의 하세요';
+	  console.log(err);
+	  res.json(result);
+	} else {
+
+		var result = new Result();
+		
+		
+		if(row.affectedRows === 1) {
+			
+			res.setHeader('Content-Type', 'application/json; charset=utf-8');
+			result.resultCode = '200';
+			result.resultMessage = '즐겨찾기 등록 되었습니다.';
+			console.log(result);
+			res.json(result);
+		} else {
+			result.resultCode = '500';
+			result.resultMessage = '즐겨 찾기 등록에 실패 했습니다.';
+			console.log(result);
+			res.json(result);
+		}
+	}
+  });
+});
+
+/* 완료 */
 router.get('/favoritesList.app', function(req, res, next) {
-  res.send('favoritesList.app');
+  	var reqParam = req.query;
+	
+	var memberId = reqParam.memberId;
+	var manageType = reqParam.manageType;
+	
+	var sqlQueryParam = [memberId,manageType];
+	var sqlQuery = "select lm.manage_id manageId, " + 
+	"(select city_name from address_city where city_code=lm.city_code) cityName , " + 
+	"(select state_name from address_state where state_code=lm.state_code) stateName " +
+	" from favorites_info fi right join location_management lm on fi.manage_id = lm.manage_id where fi.member_id=?";
+
+	if(manageType !== "all") {
+		sqlQuery += "and lm.manage_type=?";
+	}
+	
+	connection.query(sqlQuery, sqlQueryParam, function (err, row, fields) {
+				
+	var result = new Result();
+		
+	if(err){
+	  result.resultCode = '500';
+      result.resultMessage = '시스템 관리자에게 문의 하세요';
+	  res.json(result);
+	} else {
+
+		var result = new Result();
+		var dataLength = row.length;
+		
+		res.statusCode = "200";
+		res.setHeader('Content-Type', 'application/json; charset=utf-8');
+
+		var stateStr = {"resultCode":"200", "resultMessage":"조회", "favoritesList":row};
+		res.json(stateStr);
+	}
+  });
 });
 
 router.get('/pushRecordList.app', function(req, res, next) {
-  res.send('pushRecordList.app');
+	
+	
+    var reqParam = req.query;
+	
+	var memberId = reqParam.memberId;
+	
+	var sqlQueryParam = [memberId];
+	var sqlQuery = "select phi.sensor_id,phi.push_contents, phi.push_send_time, si.city_code, si.state_code "
+				+ "from push_history_info phi "
+				+ "join sensor_info si on phi.sensor_id = si.sensor_id "
+				+ "join member m on si.city_code = m.city_code "
+				+ "where m.member_id = ? ";
+	
+	connection.query(sqlQuery, sqlQueryParam, function (err, row, fields) {
+				
+	var result = new Result();
+		
+	if(err){
+	  result.resultCode = '500';
+      result.resultMessage = '시스템 관리자에게 문의 하세요';
+	  res.json(result);
+	} else {
+
+		var result = new Result();
+		
+		res.statusCode = "200";
+		res.setHeader('Content-Type', 'application/json; charset=utf-8');
+
+		var stateStr = {"resultCode":"200", "resultMessage":"조회", "pushList":row};
+		res.json(stateStr);
+	}
+  });
 });
 
 router.get('/wmList.app', function(req, res, next) {
@@ -247,10 +393,6 @@ router.get('/smInfo.app', function(req, res, next) {
 
 router.get('/smList.app', function(req, res, next) {
   res.send('smList.app');
-});
-
-router.get('/favoritesRegister.app', function(req, res, next) {
-  res.send('favoritesRegister.app');
 });
 
 router.get('/sensorInfoRegister.app', function(req, res, next) {
