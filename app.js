97896fb8284457efef6444f6c290b163b35065c1
KoreanWarrior <bbcjswo@naver.com>
2017-08-25 17:41:09 +0900

    뷰 환경 설정

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'jade');
 
 //app.engine(".vm", vm({
 //  root: __dirname + "/view"  //duplicated with views setting but required for velocity template
@@ -39,12 +39,14 @@ app.use('/api', api);
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
+  console.log(err);
   next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
+  console.log(err);
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,123 @@
 var express = require('express');
+var mysql_dbc = require('../config/db_con')();
+var connection = mysql_dbc.init();
+mysql_dbc.test_open(connection);
 var router = express.Router();
+
+function Result (resultCode, resultMessage) {
+    this.resultCode = resultCode;
+    this.resultMessage = resultMessage;
+}
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log("park");
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.post('/login', function(req, res, next) {
+  var memberId = req.body.memberId;
+  var memberPwd = req.body.memberPwd;
+
+  var sqlQueryParam = [memberId,memberPwd];
+  var sqlQuery = "select count(*) as count from member where member_id = ? and member_pwd = ? and member_delete_code='N'";
+  connection.query(sqlQuery, sqlQueryParam, function (err, row, fields) {
+    if(err){
+      res.send('error');
+    } else {
+
+      var result = new Result();
+
+      if(row[0].count === 1) {
+        result.resultCode = '200';
+        result.resultMessage = '로그인을 환영합니다.';
+        console.log("test1");
+        res.render('member/member_list');
+      } else {
+        result.resultCode = '500';
+        result.resultMessage = '회원정보가 없습니다.';
+        console.log("test2");
+        res.render('index');
+      }
+
+    }
+  });
+});
+
+router.get('/memberList', function(req, res, next) {
+console.log("memberList list");
+  var sqlQuery = "select * from member where member_delete_code='N'";
+  connection.query(sqlQuery, function (err, row, fields) {
+
+    console.log(row);
+    if(err){
+      res.render('error');
+    } else {
+      res.render('member/member_list',{"memberList":row});
+    }
+  });
+});
+
+router.get('/gmList', function(req, res, next) {
+console.log("gm list");
+  var sqlQuery = "select * from member where member_delete_code='N'";
+  connection.query(sqlQuery, function (err, row, fields) {
+    if(err){
+      console.log(err);
+      res.render('error');
+    } else {
+      console.log("Gm List Go");
+      res.render('gm/gm_list');
+    }
+  });
+});
+
+router.get('/smList', function(req, res, next) {
+console.log("sm list");
+  var sqlQuery = "select * from member where member_delete_code='N'";
+  connection.query(sqlQuery, function (err, row, fields) {
+    if(err){
+      res.render('error');
+    } else {
+      res.render('sm/sm_list');
+    }
+  });
+});
+
+router.get('/tmList', function(req, res, next) {
+console.log("tm list");
+  var sqlQuery = "select * from member where member_delete_code='N'";
+  connection.query(sqlQuery, function (err, row, fields) {
+    if(err){
+      res.render('error');
+    } else {
+      res.render('tm/tm_list');
+    }
+  });
+});
+
+router.get('/wmList', function(req, res, next) {
+console.log("wm list");
+  var sqlQuery = "select * from member where member_delete_code='N'";
+  connection.query(sqlQuery, function (err, row, fields) {
+    if(err){
+      res.render('error');
+    } else {
+      res.render('wm/wm_list');
+    }
+  });
+});
+
+router.get('/pushList', function(req, res, next) {
+console.log("push list");
+  var sqlQuery = "select * from member where member_delete_code='N'";
+  connection.query(sqlQuery, function (err, row, fields) {
+    if(err){
+      res.render('error');
+    } else {
+      res.render('push/push_list');
+    }
+  });
 });
 
 module.exports = router;
