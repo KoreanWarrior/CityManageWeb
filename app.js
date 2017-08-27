9f6109e5a8f1f0e32cd0f3260dceab34fbd64636
KoreanWarrior <bbcjswo@naver.com>
2017-08-27 11:28:39 +0900

    node js 화면 개발중

--- app.js
+++ app.js
@@ -39,19 +39,21 @@ app.use('/api', api);
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
-  console.log(err);
+  console.log("test1:");
+  console.log(err)
   next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  console.log(err);
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
+  console.log("test2:");
+  console.log(err)
   res.render('error');
 });
 
